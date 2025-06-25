2d9c34a1dfe469b09b21d590460498da5f06c0a4
ndrnmnk <ndrnmnk1@gmail.com>
2025-06-25 22:26:14 +0300

    setup.sh fix 2

--- setup.sh
+++ setup.sh
@@ -5,5 +5,8 @@ mkdir -p thumbnails;
 python3 -m venv venv;
 venv/bin/pip install -r requirements.txt;
 git clone https://github.com/ndrnmnk/deadRTSP;
-git -C deadRTSP apply -p1 deadRTSP_video_adjustment.patch;
+mv deadRTSP_video_adjustment.patch deadRTSP/;
+cd deadRTSP;
+git apply deadRTSP_video_adjustment.patch;
+cd ..;
 echo "Ourtube installed successfully! Execute <launch.py> to start it"
