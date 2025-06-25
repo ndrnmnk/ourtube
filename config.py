44dbc4450caa0244f3c344d9aa475071c470c0df
ndrnmnk <ndrnmnk1@gmail.com>
2025-06-25 22:12:45 +0300

    setup.sh fix

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-import json
+import yaml
 
 class Config:
     _instance = None
 
-    def __new__(cls, path="config.json"):
+    def __new__(cls, path="config.yaml"):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
             cls._instance._load_config(path)
@@ -11,7 +11,8 @@ class Config:
 
     def _load_config(self, path):
         with open(path, "r") as file:
-            self._config = json.loads(file.read())
+            self._config = yaml.safe_load(file)
+            self._used_ports = set()
 
     def get(self, key, default=None):
         return self._config.get(key, default)
--- setup.sh
+++ setup.sh
@@ -2,10 +2,8 @@
 
 mkdir -p videos;
 mkdir -p thumbnails;
-sudo apt update;
-sudo apt install ffmpeg;
 python3 -m venv venv;
 venv/bin/pip install -r requirements.txt;
 git clone https://github.com/ndrnmnk/deadRTSP;
-git apply deadRTSP_video_adjustment.patch;
+git -C deadRTSP apply -p1 deadRTSP_video_adjustment.patch;
 echo "Ourtube installed successfully! Execute <launch.py> to start it"
